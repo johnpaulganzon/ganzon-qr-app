a64940e3a6a7dbb8897f0d3723ad17b58fca5457
johnpaulganzon <jpganzon27@gmail.com>
2025-06-20 17:51:10 +0800

    update

---
+++ pages/auth/signin.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="d-flex align-center justify-center" style="height: 100vh;">
+        <v-card width="500"> 
+        <h1 class="d-flex align-cneter justify-center">SIGN IN PAGE</h1>
+        <v-text-field placeholder="Email" class=""></v-text-field>
+        <v-text-field label="Password"></v-text-field>
+
+
+        <v-button class="d-flex align-cneter justify-center"> log in </v-button>
+ 
+
+        </v-card>
+    </div>
+</template>
+<script>
+export default {
+    layout: "auth" 
+}
+</script>
