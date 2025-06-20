d1486da26197355a92945470b1c5a4b9844e10f5
johnpaulganzon <jpganzon27@gmail.com>
2025-06-20 18:00:47 +0800

    new update

--- pages/auth/signin.vue
+++ pages/auth/signin.vue
@@ -2,11 +2,13 @@
     <div class="d-flex align-center justify-center" style="height: 100vh;">
         <v-card width="500"> 
         <h1 class="d-flex align-cneter justify-center">SIGN IN PAGE</h1>
-        <v-text-field placeholder="Email" class=""></v-text-field>
-        <v-text-field label="Password"></v-text-field>
+        <v-form>
+        <v-text-field label="Email" outlined prepend-inner-icon="mdi-email"></v-text-field>
+        <v-text-field label="Password" outlined prepend-inner-icon="mdi-lock"></v-text-field>
 
 
-        <v-button class="d-flex align-cneter justify-center"> log in </v-button>
+        <v-btn color="primary" block> log in </v-btn>
+    </v-form>
  
 
         </v-card>
