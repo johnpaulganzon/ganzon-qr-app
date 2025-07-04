935b38220bc4786b65d34fcf636813ef197e9790
johnpaulganzon <jpganzon27@gmail.com>
2025-07-04 18:11:21 +0800

    Added nuxt auth plugins

--- pages/auth/signin.vue
+++ pages/auth/signin.vue
@@ -1,21 +1,22 @@
 <template>
     <div class="d-flex align-center justify-center" style="height: 100vh;">
-        <v-card width="500"> 
-        <h1 class="d-flex align-cneter justify-center">SIGN IN PAGE</h1>
-        <v-form>
-        <v-text-field label="Email" outlined prepend-inner-icon="mdi-email"></v-text-field>
-        <v-text-field label="Password" outlined prepend-inner-icon="mdi-lock"></v-text-field>
+        <v-card width="500" class="px-8 py-10 textcenter">
+            <h2 class="d-flex align-cneter justify-center">Log in Page</h2>
+            <v-form>
+                <v-text-field label="Email"
+                outlined rounded prepend-inner-icon ="mdi-email"></v-text-field>
+                <v-text-field label="Password"
+                outlined rounded prepend-inner-icon="mdi-lock"></v-text-field>
 
-
-        <v-btn color="primary" block> log in </v-btn>
-    </v-form>
- 
-
+                <v-btn color="blue" rounded block> Log in </v-btn>
+                <div class="d-flex align-center justify-center"> OR </div>
+                <v-btn color="red" class="text-capitalize" rounded block><v-icon left>mdi-google</v-icon>Sign in with google</v-btn>
+            </v-form>
         </v-card>
-    </div>
-</template>
-<script>
-export default {
-    layout: "auth" 
-}
-</script>
+        </div>
+        </template>
+        <script>
+        export default {
+            layout: "auth" 
+            }
+            </script>
